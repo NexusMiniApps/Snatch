02f3d02f97c5e4975033dd1950f3443fe54c38ef
Janessa <jnssay@gmail.com>
2025-02-10 15:25:15 +0800

    image added

--- src/app/bread/page.tsx
+++ src/app/bread/page.tsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
 
-import { LatestPost } from "~/components/ui/post";
 import { auth } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
 import { Button } from "~/components/ui/button";
+import Vibrant from "node-vibrant";
+import Image from "next/image";
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -16,14 +16,24 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <main className="flex min-h-screen flex-col items-center justify-center px-4 gap-y-6">
+      <div className="border-solid border-2 border-black rounded-md p-1 w-full h-60">
+          <div className="relative rounded-md w-full h-full">
+          <Image
+          className="object-cover rounded-md"
+            src="/images/coffee.jpg"
+            alt="Fresh bread"
+            fill
+          />
+          </div>
+          </div>
+
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Bread
           </h1>
 
-          </div>
       </main>
     </HydrateClient>
   );
 }
+
