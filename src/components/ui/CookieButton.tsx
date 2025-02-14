6f655086774e70dd0247ccd66a8d942aae4f71b3
jolow99 <jolow999@gmail.com>
2025-02-14 13:03:10 +0800

    Finding the missing cookie

--- src/components/ui/CookieButton.tsx
+++ src/components/ui/CookieButton.tsx
@@ -31,14 +31,11 @@ export default function CookieButton({ count, socket, onIncrement }: CookieButto
         }
     }, [isAnimating, count, onIncrement, socket]);
 
-    const cookieImagePath = process.env.NEXT_PUBLIC_VERCEL_URL 
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/misc/cookie.svg`
-        : "/misc/cookie.svg";
 
     return (
         <div className="animate-spin-slow">
             <img
-                src={cookieImagePath}
+                src='/misc/cookie.svg'
                 alt="Cookie"
                 className={`w-56 h-56 ${isPulsing ? 'animate-pulse-once' : ''}`}
                 onClick={handleClick}
