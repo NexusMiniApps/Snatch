e033e5c8950fc0d36ef65b55ad2709a2b61557f4
ZhiZhangT <zztee24@gmail.com>
2025-02-21 10:55:04 +0800

    update resultsview UI

--- src/components/views/ResultsView.tsx
+++ src/components/views/ResultsView.tsx
@@ -120,24 +120,31 @@ export function ResultsView({
         <Chat socket={socket} currentPlayerId={currentPlayerId} />
       </div>
 
-      <div className="z-10 px-2 py-2 text-center text-sm font-light">
-        {isWinner ? (
-          <span>
-            Join the Winners Telegram group at{" "}
-            <a
-              href="https://t.me/+WHVh-EWerUIxMzZl"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-600 hover:underline"
-            >
-              https://t.me/+WHVh-EWerUIxMzZl
-            </a>{" "}
-            for the event details!
-          </span>
-        ) : (
-          <span>Connect with the host for future events!</span>
-        )}
+      <div className="text-center text-sm mt-2">
+        <div className="flex flex-col items-center justify-center p-4 border border-gray-300 rounded-lg shadow-md w-full max-w-xs bg-white">
+          <h2 className="text-lg">
+            Stay tuned for future events!</h2>
+          <a 
+            href="https://t.me/+ZDCen5H4XCoxOTI1"
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+          >
+            Join Group
+          </a>
+        </div>
       </div>
+
+      {/* <div className="z-10 px-2 text-sm">
+        <a
+          href="https://t.me/+ZDCen5H4XCoxOTI1"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
+        >
+          Join our Telegram Group to stay tuned for future events!
+        </a>
+      </div> */}
 
       <div className="z-10 px-2 text-sm">
         <a
@@ -149,6 +156,7 @@ export function ResultsView({
           Help us improve - Share your feedback!
         </a>
       </div>
+      <br />
     </div>
   );
 }
